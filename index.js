3abbb767edef684e182c42d984d8760de9430238
peng <yangpeng@fubangyun.com>
2017-12-21 12:02:09 +0800

    修改

---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express')
+var proxy = require('http-proxy-middleware');
+var path = require('path');
+/**
+ * Configure proxy middleware
+ */
+var jsonPlaceholderProxy = proxy({
+    target: 'http://172.16.15.203:9393',     //代理地址
+    changeOrigin: true,  
+    ws:true,
+    pathRewrite: {
+        '^/api': ''                          //去掉'/api'
+    },
+    proxyTimeout: 60000,                     //连接超时时间
+    cookieDomainRewrite: {                   //cookie cookie待验证
+        "*": ""
+    }
+})
+
+var app = express();       
+//设置静态文件目录
+app.use(express.static(path.join(__dirname, './static')));
+
+/**
+ * Add the proxy to express
+ */
+app.use('/api/*', jsonPlaceholderProxy);             //匹配api下的进行代理
+//主页面
+app.get('/index', function (req, res, next) {        //主页面，可以添加更多页面
+    res.sendfile('./static/index.html');
+})
+
+app.listen(3077);                                    //开启端口
+
+console.log('start success');
---
+++ readme.md
@@ -0,0 +1,18 @@
+## web开发代理server
+
+```
+npm install
+npm run dev
+
+
+ 项目中采用了pm2运行项目,也可以采用pm2启动项目
+ 运行方式：pm2 start index.js --watch
+```
+
+### 2017-12-18
+```
+ 1.创建工程，实现代理。
+ 2.静态代码存放在web_page中
+ 3.在接口请求的时候在接口地址前加上‘/api’
+```
+
